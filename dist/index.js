102528031de2061d39f97e90aaefe48cb90714f7
François-Xavier P. Darveau <frxpoulin@gmail.com>
2016-10-13 21:18:23 -0400

    added integration section

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,6 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// import * as slacker from './slacker';
-
 var CONTACT_FORM_EVENT = 'Contact form sent';
 
 var getFormValuesObject = function getFormValuesObject(formEl) {
