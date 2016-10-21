cc866002425bc11cbffccef01324736b82b768a5
osylvain <g.oliviersylvain@gmail.com>
2016-10-21 17:26:27 -0400

    the QA tour

--- dist/index.js
+++ dist/index.js
@@ -181,11 +181,11 @@ window.setTimeout(function () {
     $('#page-loader').addClass('fadeOut');
 }, timeout);
 
-var BG_IMAGE_IDS = ['1471015060382-6cbd8b4e34d0', '1450027179084-b3ff1fce4bb5', '1471565661762-b9dfae862dbe', '1442810480970-6d3fc310e3eb'];
+var BG_IMAGE_FILENAMES = ['OP_Image01.jpg', 'OP_Image02.jpg', 'OP_Image03.jpg', 'OP_Image04.jpg'];
 
 var setRandomBackgroundImage = function setRandomBackgroundImage() {
-    var randomImageId = BG_IMAGE_IDS[Math.floor(Math.random() * BG_IMAGE_IDS.length)];
-    var randomImageCSSString = 'background-image: url(https://images.unsplash.com/photo-' + randomImageId + '?dpr=2&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb)';
+    var randomImage = BG_IMAGE_FILENAMES[Math.floor(Math.random() * BG_IMAGE_FILENAMES.length)];
+    var randomImageCSSString = 'background-image: url(' + originUrl + '/dist/img/' + randomImage + ')';
 
     document.getElementById('header').setAttribute('style', randomImageCSSString);
 };
