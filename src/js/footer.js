8da9ed01ddeda71ca432e56f5ba5397ba34848b1
osylvain <g.oliviersylvain@gmail.com>
2016-12-06 11:49:51 -0500

    file format

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,18 +1,18 @@
 class Footer {
-	constructor() {
-		this.sitemapItems = $('footer .sitemap a');
-		this.sitemapItems.click(this.handleSitemapClick);
+    constructor() {
+        this.sitemapItems = $('footer .sitemap a');
+        this.sitemapItems.click(this.handleSitemapClick);
 
-	}
+    }
 
-	handleSitemapClick(e) {
-		const sectionId = $(e.currentTarget).attr('href');
-		const sectionOffset = getSectionOffset(sectionId);
+    handleSitemapClick(e) {
+        const sectionId = $(e.currentTarget).attr('href');
+        const sectionOffset = getSectionOffset(sectionId);
 
-		$('html, body').stop().animate({
-			scrollTop: sectionOffset
-		}, GET_STARTED_ANIMATION_DURATION);
+        $('html, body').stop().animate({
+            scrollTop: sectionOffset
+        }, GET_STARTED_ANIMATION_DURATION);
 
-		e.preventDefault();
-	}
+        e.preventDefault();
+    }
 }
